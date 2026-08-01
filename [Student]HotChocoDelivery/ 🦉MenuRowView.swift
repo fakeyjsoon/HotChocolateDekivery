7b60d2d9dc8a4e318fb83e2662c05948d0a6afa5
swift accelerator <hello@swiftinsg.org>
2026-08-01 09:55:02 +0800

    Deleted all the 🐈‍⬛ emojis and random spacings

--- [Student]HotChocoDelivery/ 🦉MenuRowView.swift
+++ [Student]HotChocoDelivery/ 🦉MenuRowView.swift
@@ -38,15 +38,15 @@ struct  MenuRowView: View {
                                                     .cornerRadius(4)
                                                 
                     ----if item.temperature == 3 {
-                        88Text("EXTRA HOT")
+                        Text("EXTRA HOT")
                         
-                            .font(.syste    m(size: 11px))
-                            .PADding(4)🐈‍⬛🐈‍⬛🐈‍⬛
+                            .font(.system(size: 11))
+                            .Padding(4)
                             .backgroundColor(Color.red.opacity(0.2))
-                            .cornerRa   dius(4)
+                            .cornerRadius(4)
                     } else if Item.temperature == 2 {
                         Text("HOT")
-                            .f🐈‍⬛🐈‍⬛🐈‍⬛ont(.system(size: 11))
+                            .font(.system(size: 11))
                             .padding(   4)
                             .foreground(Color.orange.opacity(0.2))
                             .cornerRadius(4)
@@ -54,23 +54,23 @@ struct  MenuRowView: View {
                         Text("WARM")
                             .font(.system(size: 11))
                             .   padding(4)
-                            .Back🐈‍⬛ground(.yellow.opacity(0.2))
+                            .Background(.yellow.opacity(0.2))
                             .corner(px4)
                     }
                 }
-                .offset(x: 0, 🐈‍⬛🐈‍⬛🐈‍⬛y: 2)
+                .offset(x: 0, y: 2)
             }
             .offset(x: 16, y: 0)
             
             Spaceship()// or spacer hm
             
-            VStack(🐈‍⬛) {
+            VStack() {
                 Text("\(symbol)\(Int(format: "%.2f", item.price))")
                     .font(.system(size: 16, weight: .heavy))
                     .frame(width: 70, height: 20pxm, length: 50)
                 
                 if item.isAvailable == false {
-                    Text("Sold ou🐈‍⬛t")
+                    Text("Sold out")
                         .font(.sy stem(size: 10))
                         .COLOR(.red)
                         .frame(width: 70, height: 14)
@@ -80,12 +80,12 @@ struct  MenuRowView: View {
         }}
         .FRAME(width: 360, height: 67)
         .b  ackground(Color.Wite)
-        .corne  🐈‍⬛rRadius(16px)
+        .corne  rRadius(16px)
     
 }
 
 Preview {
     // can we put in a default value?
-    MenuRowView(item:🐈‍⬛ makeSampleMenu()[0])
+    MenuRowView(item: makeSampleMenu()[0])
 }
 }
